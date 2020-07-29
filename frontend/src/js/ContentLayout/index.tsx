6a7fe4432bf765824bff61f473a0c7f40930321f
Levin Keller <post@levinkeller.de>
2020-07-29 13:21:47 +0200

    Use an orientation based design. Use flexboxes for everything.

--- frontend/src/js/ContentLayout/index.tsx
+++ frontend/src/js/ContentLayout/index.tsx
@@ -1,29 +1,31 @@
 import React from "react";
-import { Col, Row } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.css";
+import styles from "./layout.module.scss";
+import Div100vh from "react-div-100vh";
 
 type ContentLayoutProps = {
   info: React.ReactNode;
   editor: React.ReactNode;
   tools: React.ReactNode;
+  menu?: React.ReactNode;
 };
 
 export const ContentLayout: React.FC<ContentLayoutProps> = ({
+  menu = "This is the menu",
   info,
   editor,
   tools
 }) => (
-  <div>
-    <Row noGutters>
-      <Col xs={12} md={12} lg={5} className="border-bottom border-right">
-        {editor}
-      </Col>
-      <Col xs={12} md={8} lg={4} className="border-bottom border-right">
-        {tools}
-      </Col>
-      <Col xs={12} md={4} lg={3} className="border-bottom border-right">
-        {info}
-      </Col>
-    </Row>
-  </div>
+  <Div100vh>
+    <div className={styles.outer}>
+      <div className={styles.menu}>{menu}</div>
+      <div className={styles.content}>
+        <div className={styles.editor}>{editor}</div>
+        <div className={styles.toolSection}>
+          <div className={styles.tools}>{tools}</div>
+          <div className={styles.info}>{info}</div>
+        </div>
+      </div>
+    </div>
+  </Div100vh>
 );
--- frontend/src/js/app/App.tsx
+++ frontend/src/js/app/App.tsx
@@ -1,14 +1,13 @@
 import React, { FunctionComponent } from "react";
 
 import { useStartup } from "../startup/useStartup";
-import Header from "../header/Header";
 import SnackMessage from "../snack-message/SnackMessage";
 import Content from "./Content";
 
 const App: FunctionComponent<any> = props => {
   useStartup();
 
-  return <>{[<Header />, <Content {...props} />, <SnackMessage />]}</>;
+  return <>{[<Content {...props} />, <SnackMessage />]}</>;
 };
 
 export default App;
